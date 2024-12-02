f9848cd5537298bbfdde1a0f83d474895af5f25c
Michael Cole <michaelcole239@gmail.com>
2024-12-02 22:40:00 +0000

    Set up days.py

---
+++ aoc24/day7.py
@@ -0,0 +1,50 @@
+def parse_day7_input(user_input: list[str]):
+    """
+    Function to parse the user input for Day 7.
+    Args:
+        user_input (list[str]): The user input.
+    Returns:
+
+    """
+    pass
+
+
+def solve_day7_puzzle_part1(user_input: list[str]) -> int:
+    """
+    Function to solve part 1 of the puzzle for Day 7.
+    Args:
+        user_input (list[str]): The user input.
+    Returns:
+        int: The solution to the puzzle (part 1)
+    """
+    # Parse user input
+    pass
+
+
+def solve_day7_puzzle_part2(user_input: list[str]) -> int:
+    """
+    Function to solve part 1 of the puzzle for Day 7.
+    Args:
+        user_input (list[str]): The user input.
+    Returns:
+        int: The solution to the puzzle (part 2)
+    """
+    # Parse user input
+    pass
+
+
+def solve_day7_puzzle(user_input: list[str], part: int) -> int:
+    """
+    Function to solve the puzzle for Day 7.
+    Args:
+        user_input (list[str]): The user input.
+        part (int): The part of the puzzle to solve.
+    Returns:
+        int: The solution to the puzzle for the given part.
+    """
+    if part == 1:
+        return solve_day7_puzzle_part1(user_input)
+    elif part == 2:
+        return solve_day7_puzzle_part2(user_input)
+    else:
+        raise ValueError(f"Invalid value {part} for part. Part must be 1 or 2.")
--- aoc24/utils.py
+++ aoc24/utils.py
@@ -1,8 +1,57 @@
 from aoc24.day1 import solve_day1_puzzle
 from aoc24.day2 import solve_day2_puzzle
+from aoc24.day3 import solve_day3_puzzle
+from aoc24.day4 import solve_day4_puzzle
+from aoc24.day5 import solve_day5_puzzle
+from aoc24.day6 import solve_day6_puzzle
+from aoc24.day7 import solve_day7_puzzle
+from aoc24.day8 import solve_day8_puzzle
+from aoc24.day9 import solve_day9_puzzle
+from aoc24.day10 import solve_day10_puzzle
+from aoc24.day11 import solve_day11_puzzle
+from aoc24.day12 import solve_day12_puzzle
+from aoc24.day13 import solve_day13_puzzle
+from aoc24.day14 import solve_day14_puzzle
+from aoc24.day15 import solve_day15_puzzle
+from aoc24.day16 import solve_day16_puzzle
+from aoc24.day17 import solve_day17_puzzle
+from aoc24.day18 import solve_day18_puzzle
+from aoc24.day19 import solve_day19_puzzle
+from aoc24.day20 import solve_day20_puzzle
+from aoc24.day21 import solve_day21_puzzle
+from aoc24.day22 import solve_day22_puzzle
+from aoc24.day23 import solve_day23_puzzle
+from aoc24.day24 import solve_day24_puzzle
+from aoc24.day25 import solve_day25_puzzle
 from datetime import datetime, date
 
-FUNCS = [solve_day1_puzzle, solve_day2_puzzle]
+FUNCS = [
+    solve_day1_puzzle,
+    solve_day2_puzzle,
+    solve_day3_puzzle,
+    solve_day4_puzzle,
+    solve_day5_puzzle,
+    solve_day6_puzzle,
+    solve_day7_puzzle,
+    solve_day8_puzzle,
+    solve_day9_puzzle,
+    solve_day10_puzzle,
+    solve_day11_puzzle,
+    solve_day12_puzzle,
+    solve_day13_puzzle,
+    solve_day14_puzzle,
+    solve_day15_puzzle,
+    solve_day16_puzzle,
+    solve_day17_puzzle,
+    solve_day18_puzzle,
+    solve_day19_puzzle,
+    solve_day20_puzzle,
+    solve_day21_puzzle,
+    solve_day22_puzzle,
+    solve_day23_puzzle,
+    solve_day24_puzzle,
+    solve_day25_puzzle,
+]
 
 
 def read_input(day: int):
