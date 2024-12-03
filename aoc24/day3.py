2fd6990a02b21f67fdcf0325c97872ad93baba5a
Michael Cole <michaelcole239@gmail.com>
2024-12-03 06:45:19 +0000

    Day 3 solutions

--- aoc24/day3.py
+++ aoc24/day3.py
@@ -1,43 +1,59 @@
-def parse_day3_input(user_input: list[str]):
-    """
-    Function to parse the user input for Day 3.
-    Args:
-        user_input (list[str]): The user input.
-    Returns:
-
-    """
-    pass
+from re import findall
 
 
-def solve_day3_puzzle_part1(user_input: list[str]) -> int:
+def solve_day3_puzzle_part1(user_input: str) -> int:
     """
     Function to solve part 1 of the puzzle for Day 3.
     Args:
-        user_input (list[str]): The user input.
+        user_input (str): The user input.
     Returns:
         int: The solution to the puzzle (part 1)
     """
-    # Parse user input
-    pass
+    # Find all patterns of the form "mul(a,b)"
+    found_patterns = findall(r"mul\(\d+,\d+\)", user_input)
+
+    # Calculate the sum of the products
+    total = 0
+    for pattern in found_patterns:
+        num1, num2 = [int(x) for x in pattern.strip("mul")[1:-1].split(",")]
+        total += num1 * num2
+
+    return total
 
 
-def solve_day3_puzzle_part2(user_input: list[str]) -> int:
+def solve_day3_puzzle_part2(user_input: str) -> int:
     """
     Function to solve part 1 of the puzzle for Day 3.
     Args:
-        user_input (list[str]): The user input.
+        user_input (str): The user input.
     Returns:
         int: The solution to the puzzle (part 2)
     """
-    # Parse user input
-    pass
+    # Find all patterns of the form "mul(a,b)", "do", and "don't"
+    patterns = findall("mul\(\d+,\d+\)|don't|do(?!xy)", user_input)
+
+    # Keep track of enabled status
+    enabled = True
+    total = 0
+
+    # Calculate the sum of the products, only if enabled
+    for pattern in patterns:
+        if "mul" in pattern and enabled:
+            num1, num2 = [int(x) for x in pattern.strip("mul()").split(",")]
+            total += num1 * num2
+        elif pattern == "do":
+            enabled = True
+        elif pattern == "don't":
+            enabled = False
+
+    return total
 
 
-def solve_day3_puzzle(user_input: list[str], part: int) -> int:
+def solve_day3_puzzle(user_input: str, part: int) -> int:
     """
     Function to solve the puzzle for Day 3.
     Args:
-        user_input (list[str]): The user input.
+        user_input (str): The user input.
         part (int): The part of the puzzle to solve.
     Returns:
         int: The solution to the puzzle for the given part.
@@ -48,11 +64,3 @@ def solve_day3_puzzle(user_input: list[str], part: int) -> int:
         return solve_day3_puzzle_part2(user_input)
     else:
         raise ValueError(f"Invalid value {part} for part. Part must be 1 or 2.")
-
-
-sum(
-    [
-        prod([int(num) for num in pattern.strip("mul")[1:-1].split(",")])
-        for pattern in findall(r"mul\(\d+,\d+\)", user_input)
-    ]
-)
