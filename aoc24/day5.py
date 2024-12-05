4b6a29d221a5cdd119ce5bb13e344c664b845c45
Michael Cole <michaelcole239@gmail.com>
2024-12-05 23:45:05 +0000

    Day 5 solutions

--- aoc24/day5.py
+++ aoc24/day5.py
@@ -1,3 +1,8 @@
+from collections import defaultdict
+from itertools import combinations
+from graphlib import TopologicalSorter
+
+
 def parse_day5_input(user_input: str):
     """
     Function to parse the user input for Day 5.
@@ -6,7 +11,27 @@ def parse_day5_input(user_input: str):
     Returns:
 
     """
-    pass
+    user_input = user_input.split("\n")
+
+    lists = []
+    orders = defaultdict(list)
+    for line in user_input:
+        if "|" in line:
+            num1, num2 = line.split("|")
+            orders[int(num1)].append(int(num2))
+        elif line == "":
+            continue
+        else:
+            lists.append([int(x) for x in line.split(",")])
+
+    return lists, orders
+
+
+def is_line_ordered(line: list[int], orders: dict[int, int]) -> bool:
+    for num1, num2 in combinations(line, 2):
+        if num1 in orders[num2]:
+            return False
+    return True
 
 
 def solve_day5_puzzle_part1(user_input: str) -> int:
@@ -18,7 +43,14 @@ def solve_day5_puzzle_part1(user_input: str) -> int:
         int: The solution to the puzzle (part 1)
     """
     # Parse user input
-    pass
+    lines, orders = parse_day5_input(user_input)
+
+    # Find the middle element of the sorted line
+    total = 0
+    for line in lines:
+        if is_line_ordered(line, orders):
+            total += line[len(line) // 2]
+    return {"solution": total}
 
 
 def solve_day5_puzzle_part2(user_input: str) -> int:
@@ -30,7 +62,19 @@ def solve_day5_puzzle_part2(user_input: str) -> int:
         int: The solution to the puzzle (part 2)
     """
     # Parse user input
-    pass
+    lines, orders = parse_day5_input(user_input)
+
+    # Use topological sort to find the middle element of the sorted line
+    total = 0
+    for line in lines:
+        if not is_line_ordered(line, orders):
+            these_orders = {
+                k: [i for i in v if i in line] for k, v in orders.items() if k in line
+            }
+            sorted_line = list(TopologicalSorter(these_orders).static_order())[::-1]
+            total += sorted_line[len(sorted_line) // 2]
+
+    return {"solution": total}
 
 
 def solve_day5_puzzle(user_input: str, part: int) -> int:
